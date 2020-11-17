2126056688ebeeeacaf43dbf50b57bd408accdbe
Zack Colton <coltonz@stpaulsschool.org.uk>
2020-11-17 13:45:01 +0000

    Got Firestore working again

--- lib/backend/firestoreBackend.dart
+++ lib/backend/firestoreBackend.dart
@@ -1,19 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void addUserDetails() async {
 //https://medium.com/@atul.sharma_94062/how-to-use-cloud-firestore-with-flutter-e6f9e8821b27
 }
 
 Future<int> eventCount(String userId) async {
-  final databaseReference = Firestore.instance;
+  await Firebase.initializeApp();
+  final databaseReference = FirebaseFirestore.instance;
   //https://stackoverflow.com/questions/54456665/how-to-count-the-number-of-documents-firestore-on-flutter
   QuerySnapshot eventsSnapshot = await databaseReference
       .collection("users")
-      .document(userId)
+      .doc(userId)
       .collection("events")
-      .getDocuments();
+      .get();
 
-  List<DocumentSnapshot> events = eventsSnapshot.documents;
-
+  List<DocumentSnapshot> events = eventsSnapshot.docs;
+  print(events.length);
   return events.length;
 }
--- lib/backend/webScraperBackend.dart
+++ lib/backend/webScraperBackend.dart
@@ -1,16 +1,7 @@
-import 'package:web_scraper/web_scraper.dart';
 import '../objects/event.dart';
 
 //https://medium.com/flutter/the-power-of-webviews-in-flutter-a56234b57df2
 //https://medium.com/flutter-community/inappwebview-the-real-power-of-webviews-in-flutter-c6d52374209d
-
-Future<String> getTimetable() async {
-  final webScraper = WebScraper("https://intranet.stpaulsschool.org.uk");
-  if (await webScraper.loadWebPage('/dashboard')) {
-    // List<Map<String, dynamic>> elements = webScraper.printWrapped(elements);
-  }
-  return null;
-}
 
 Future<String> loginToFirefly() async {
   return null;
