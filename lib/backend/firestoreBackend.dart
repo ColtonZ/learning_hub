84493ae7ff871177104cdf47bb3da4547c129143
Zack Colton <coltonz@stpaulsschool.org.uk>
2020-11-18 17:07:41 +0000

    Parsed webscraped details

--- lib/backend/firestoreBackend.dart
+++ lib/backend/firestoreBackend.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:learning_hub/backend/helperBackend.dart';
+import 'dart:core';
 
 Future<int> fireflyEventCount(String id) async {
   await Firebase.initializeApp();
@@ -61,9 +62,57 @@ void addFirestoreEvents(String eventsText, String week, String id) async {
         .delete();
   });
 
-  printWrapped(eventsText);
+  List<String> weeks = DateTime.now().weekday == 1
+      ? week == "A"
+          ? ["A", "B"]
+          : ["B", "A"]
+      : week == "A"
+          ? ["B", "A"]
+          : ["A", "B"];
 
   List<String> eventsTextList = eventsText.split("; ");
+
+  int day = 0;
+  int prevStartTime = 0;
+
+  eventsTextList.forEach((eventString) {
+    List<String> eventDetails = eventString.split(", ");
+    List<int> timings = [];
+    List<String> compoundDetails;
+    String classSet;
+    String room;
+
+    if (eventDetails[2].contains(" in ")) {
+      compoundDetails = eventDetails[2].split(" in ");
+
+      List<String> roomAndSet = compoundDetails[1].split(" (");
+      room = roomAndSet[0];
+
+      if (roomAndSet[1].contains("-/")) {
+        classSet = roomAndSet[1].substring(0, roomAndSet[1].indexOf("-/"));
+      } else {
+        classSet = roomAndSet[1].replaceAll(")", "");
+      }
+    } else {
+      compoundDetails = eventDetails[2].split(" (");
+
+      if (compoundDetails[1].contains("-/")) {
+        classSet =
+            compoundDetails[1].substring(0, compoundDetails[1].indexOf("-/"));
+      } else {
+        classSet = compoundDetails[1].replaceAll(")", "");
+      }
+    }
+
+    List<String> timingsStrings = compoundDetails[0].split(" - ");
+
+    timingsStrings.forEach((time) {
+      timings.add(int.parse(time.replaceAll(":", "")));
+    });
+
+    print(
+        "Class: ${eventDetails[0]} | Teacher: ${eventDetails[1]} | Start Time: ${timings[0]} | End Time: ${timings[1]} | Room: $room | Set: $classSet");
+  });
 
   //check if lesson added (same room, teacher, subject), then check if time already there, then check repitions
 }
